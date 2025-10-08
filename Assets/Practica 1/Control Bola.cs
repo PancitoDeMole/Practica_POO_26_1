56352555420f5e26f6d64e6f6f728da950fe2768
PancitoDeMole <maximilianotrabmex@gmail.com>
2025-10-07 20:55:34 -0600

    Add files via upload

--- Assets/Practica 1/Control Bola.cs
+++ Assets/Practica 1/Control Bola.cs
@@ -1,11 +1,21 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlBola : MonoBehaviour
 {
+    public Transform CamaraPrincipal;
+
     public Rigidbody rb;
-    public float fuerza = 3000f;
+    //variable para apuntar
+    public float velocidadDEApuntado = 5f;
+    public float limiteIzquierdo = -2f;
+    public float limiteDerecho = 2f;
+
+    private float fuerza = 10000f;
+
+    private bool hasidolanzada = false;
     
     void Start()
     {
@@ -13,11 +23,42 @@ public class ControlBola : MonoBehaviour
     }
 
     void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space)) //es para llamar a una tacla
+    {       //Todo lo que esta dentro del if es una "Expresión"
+        if ( hasidolanzada==false)
         {
-            rb.AddForce(Vector3.forward * fuerza); //es para agregar una fuerza hacia "delante/formard"
-            //Debug.Log("Salto"); // sirve para imprimir un mensaje
+            Apuntar();
+
+            if (Input.GetKeyDown(KeyCode.Space)) //es para llamar a una tacla
+            {
+                Lanzar();
+            }
         }
     }
-}
+
+    void Lanzar()
+    {
+        hasidolanzada = true;
+        rb.AddForce(Vector3.forward * fuerza); //es para agregar una fuerza hacia "delante/formard"
+        //Debug.Log("Salto"); // sirve para imprimir un mensaje
+
+        if(CamaraPrincipal != null)
+        {
+            CamaraPrincipal.SetParent(transform);
+        }
+    }
+
+    void Apuntar()
+    {
+        //El input Horizontal tipo Axis: me permite registrar entredas con la tecla A y D / Flechas
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        //me permite mover la bola hacia los lados
+        transform.Translate(Vector3.right * inputHorizontal * velocidadDEApuntado * Time.deltaTime);
+        //transform.position: me permite saber cual es la posicion actual de la bola en la escena
+        Vector3 posicionActual = transform.position;
+
+        //Mathf: estoy acotando que tanto puedop moverme de izquierda a la derecha
+        posicionActual.x = Mathf.Clamp(posicionActual.x, limiteIzquierdo, limiteDerecho); 
+        transform.position = posicionActual;
+    }
+
+}//NO TE PASES DE AQUI ESTUPIDO >:(
